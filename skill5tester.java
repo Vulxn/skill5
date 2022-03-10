f4d767e4337c8cf364ac3104cba4b475101e4ce3
Vulxn <walden@ucsd.edu>
2022-03-09 21:31:17 -0800

    p

---
+++ skill5tester.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class skill5tester {
+    @Test
+    public void test1() {
+        assertEquals(2, skillDemonstrator.multiplication(4, 2));
+    }
+}
---
+++ skillDemonstrator.java
@@ -0,0 +1,5 @@
+public class skillDemonstrator {
+    public static int multiplication(int x, int y) {
+        return x * y;
+    }
+}
